6ad2ed41908af851301e30a6124731c32c4a9f94
Mateen Ali <mateen@phonotech.ca>
2025-12-21 03:50:25 -0500

    Add handler print statements for debugging

--- handler.py
+++ handler.py
@@ -3,8 +3,14 @@ AWS Lambda handler for CICosts API.
 
 Uses Mangum to adapt FastAPI ASGI app for Lambda.
 """
+print("[HANDLER] Loading CICosts API Lambda handler")
+
 from mangum import Mangum
 from app.main import app
 
+print("[HANDLER] Creating Mangum adapter")
+
 # Lambda handler
 handler = Mangum(app, lifespan="off")
+
+print("[HANDLER] Handler ready")
